961865cc4d63470032b89af46dd0059c1b90b669
wissem hajbi <wissemhajbi2002@gmail.com>
2022-03-16 20:36:33 +0100

    Remove a hardcoded Django secret key

--- Ithink2/settings.py
+++ Ithink2/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--d0n1zea-66_g!(b4yqg)fg*)!(x=9et71ss29*0_-xsgfg*n8'
+# SECRET_KEY = 'django-insecure--d0n1zea-66_g!(b4yqg)fg*)!(x=9et71ss29*0_-xsgfg*n8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
